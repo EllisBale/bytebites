1b4d04f7b6e97b5e47e604b0bb365f1ea9801ed4
EllisBale <elscar29@gmail.com>
2026-07-16 01:14:04 +0100

    style: add colour to form and adjust layout on hero section

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -5,13 +5,13 @@ import PostcodeChecker from "./PostcodeChecker";
 const Hero = () => {
     return (
         <header className="bg-[#15803d] text-white">
-            <div className="w-full p-20 bg-cover bg-center bg-no-repeat h-svh bg-[image:var(--hero-bg)] max-[1100px]:bg-none"
+            <div className="w-full p-3 lg:p-20 bg-cover bg-center bg-no-repeat h-svh bg-[image:var(--hero-bg)] max-[1100px]:bg-none"
                 style={{
                    "--hero-bg": `url(${heroBg})`,
                 } as React.CSSProperties}>
                 <div className="flex flex-col items-start max-[1100px]:items-center justify-center h-full w-full text-center lg:pl-10">
-                    <h1 className="text-6xl font-bold mb-10 w-100 text-center"> ByteBites</h1>
-                    <p className="text-lg font-bold mb-10 w-100 text-center"> Order Fresh Food Fast</p>
+                    <h1 className="text-6xl font-bold mb-10 w-full lg:w-100 text-center"> ByteBites</h1>
+                    <p className="text-lg font-bold mb-10 w-full lg:w-100 text-center"> Order Fresh Food Fast</p>
                     <PostcodeChecker />
                 </div>
             </div>
--- client/src/components/PostcodeChecker.tsx
+++ client/src/components/PostcodeChecker.tsx
@@ -22,21 +22,30 @@ const PostcodeChecker = () => {
     };
 
     return(
-        <div className="w-100 text-center">
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Enter your postcode"
-                    value={postcode}
-                    onChange={(e) => setPostcode(e.target.value)}
-                />
-                <button type="submit">
-                    Order Now
-                </button>
-            </form>
+        <div className="flex flex-col w-full text-center">
+            <div className="w-full">
+                <form 
+                    onSubmit={handleSubmit}
+                    className="flex justify-center min-[1100px]:justify-start w-fit mx-auto min-[1100px]:mx-0 bg-white"
+                
+                >
+                    <input 
+                        type="text" 
+                        placeholder="Enter your postcode"
+                        value={postcode}
+                        onChange={(e) => setPostcode(e.target.value)}
+                        className="p-3 text-black"
+                    />
+                    <button 
+                        type="submit"
+                        className="p-3 cursor-pointer bg-[#dc2626] hover:bg-[#b01e1e] shadow-md"
+                        >
+                        Order Now
+                    </button>
+                </form>
 
-            {error && <p>{error}</p>}
-
+                {error && <p className="p-2 flex justify-start w-full">{error}</p>}
+            </div>
         </div>
     )
 };
